912595bfdbd737abd2590b2e7121ee7c5978ddcd
xiangxue <460080137@qq.com>
2020-01-09 09:44:23 +0800

    <fix>package.json&&loacal

--- src/api/local.js
+++ src/api/local.js
@@ -1,6 +1,7 @@
 
 export const _local = 'http://47.99.177.201:8902/ysy'; //院外-外网
- //export const FTP = 'http://47.99.177.201:8902/ysy/ftp'//图片预览
-// export const _local = 'http://ywtest.hsms.com.cn'; 
- export const FTP = 'http://ywtest.hsms.com.cn/ftp'
+//export const FTP = 'http://47.99.177.201:8902/ysy/ftp'//图片预览
+// export const _local = 'http://ywtest.hsms.com.cn';
+export const FTP = 'http://ywtest.hsms.com.cn/ftp'
 //export const _local = 'http://192.168.31.188:8080/ysy'
+export const MEQM = 'http://wxtest.hsms.com.cn/meqm' ;  //扫码
